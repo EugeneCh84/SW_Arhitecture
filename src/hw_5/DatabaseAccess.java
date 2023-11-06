805f28591f250ea5741d76fcbf38cba23fa478a8
eugene <eugene.cherepanov.84@gmail.com>
2023-11-06 15:49:31 +0500

    1

---
+++ src/hw_5/DatabaseAccess.java
@@ -0,0 +1,17 @@
+package hw_5;
+
+import java.util.Collection;
+
+/**
+ * Интерфейс DAC
+ */
+public interface DatabaseAccess {
+
+    void addEntity(Entity entity);
+
+    void removeEntity(Entity entity);
+
+    Collection<Texture> getAllTextures();
+
+    Collection<Model3D> getAllModels();
+}
---
+++ src/hw_5/Entity.java
@@ -0,0 +1,8 @@
+package hw_5;
+
+/**
+ * Сущьность
+ */
+public interface Entity {
+    int getId();
+}
